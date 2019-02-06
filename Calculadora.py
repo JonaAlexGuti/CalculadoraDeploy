73180ffb8112642ffeceb383901f6a267d7471f1
JonaAlexGuti <jonathanalejandro@gmail.com>
2019-02-06 09:12:34 -0600

    Se corrigieron errores logicos en el test

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,3 @@
-import unittest
 class Calculadora(object):
 	def suma(self, num1, num2):
 		return num1+num2
--- Test_Calculadora.py
+++ Test_Calculadora.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 from Calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
@@ -8,7 +9,7 @@ class TestCalculadora(unittest.TestCase):
 
 	def test_suma_menos_5_mas_10(self):
 		cal=Calculadora()
-		self.assertEquals(5,cal.suma(-10,5))
+		self.assertEquals(5,cal.suma(-5,10))
 
 if __name__ == '__main__':
 	unittest.main()
