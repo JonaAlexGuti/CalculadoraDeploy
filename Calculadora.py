881849ad004f6654a78dc023d55516ad24ade954
JonaAlexGuti <jonathanalejandro@gmail.com>
2019-02-06 09:07:10 -0600

    Agrega una nueva prueba

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,4 @@
+import unittest
 class Calculadora(object):
 	def suma(self, num1, num2):
 		return num1+num2
--- Test_Calculadora.py
+++ Test_Calculadora.py
@@ -1,4 +1,3 @@
-import unittest
 from unittest import TestCase
 from Calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
@@ -6,6 +5,10 @@ class TestCalculadora(unittest.TestCase):
 	def test_suma_2_mas_2(self):
 		cal=Calculadora()
 		self.assertEquals(4,cal.suma(2,2))
+
+	def test_suma_menos_5_mas_10(self):
+		cal=Calculadora()
+		self.assertEquals(5,cal.suma(-10,5))
 
 if __name__ == '__main__':
 	unittest.main()
