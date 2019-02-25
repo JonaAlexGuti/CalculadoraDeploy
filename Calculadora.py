763b16415b6d3a476a221f146ba266611a0c4acf
JonaAlexGuti <jonathanalejandro@gmail.com>
2019-02-25 08:47:54 -0600

    corrección de codigo y adicion del documento requirements

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,3 @@
 class Calculadora(object):
-	def suma(self, num1, num2):
-		return num1+num2
+    def suma(self, num1, num2):
+        return num1+num2
--- Test_Calculadora.py
+++ Test_Calculadora.py
@@ -1,15 +1,17 @@
 import unittest
-from unittest import TestCase
+'#from unittest import TestCase'
 from Calculadora import Calculadora
-class TestCalculadora(unittest.TestCase):
-	
-	def test_suma_2_mas_2(self):
-		cal=Calculadora()
-		self.assertEquals(4,cal.suma(2,2))
 
-	def test_suma_menos_5_mas_10(self):
-		cal=Calculadora()
-		self.assertEquals(5,cal.suma(-5,10))
+
+class TestCalculadora(unittest.TestCase):
+    def test_suma_2_mas_2(self):
+        cal = Calculadora()
+        self.assertEquals(4, cal.suma(2, 2))
+
+    def test_suma_menos_5_mas_10(self):
+        cal = Calculadora()
+        self.assertEquals(5, cal.suma(-5, 10))
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
